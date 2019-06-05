24a00ffd3881d77f2d11b3fd99f1e74c2a0c8d47
mariana-ramos <44540633+mariana-ramos@users.noreply.github.com>
2019-06-04 22:18:46 -0300

    Update TrabSO.c
    Foi trabalhada a parte de entrada e saída de arquivos

--- TrabSO.c
+++ TrabSO.c
@@ -3,24 +3,26 @@
 #include <pthread.h>
 #define MAX 1000
 /*-------------------------------------------------------------------------------------------*/
-//int A[MAX][MAX], B[MAX][MAX], C[MAX][MAX];
-//int NumThread;
-int N; // dimensıes da matriz
-int T; // Numero de threads 
+double A[MAX][MAX], B[MAX][MAX], C[MAX][MAX];
+int NumThread;
+int N; // dimens√µes da matriz
+int T; // Numero de threads
+char NomeArq[100];// nome do arquivo
+
 
 /*-------------------------------------------------------------------------------------------*/
 
 void *Div1(void *arg){  // Divide a matriz A e coloca a diagonal principal e acima na matriz B;
     for(int i = 0; i<N;i++){
         for(int j=0;j<N;j++){
-            
+
             if(j>i || j==i){
-                B[i][j] = A[i][j];  
+                B[i][j] = A[i][j];
             }
             else{
                 B[i][j] = 0;
             }
-            
+
         }
     }
 }
@@ -28,43 +30,76 @@ void *Div1(void *arg){  // Divide a matriz A e coloca a diagonal principal e aci
 void *Div2(void *arg){  // Divide a matriz a e coloca a diagonal principal e abaixo na matriz C;
 	 for(int i = 0; i<N;i++){
         for(int j=0;j<N;j++){
-            
+
             if(i>j && i!=j){
-                C[i][j] = A[i][j];  
+                C[i][j] = A[i][j];
             }
             else{
                 C[i][j] = 0;
             }
-            
+
         }
-    }	
-	
+    }
+
+}
+
+void GravaMatriznoArquivo(){
+    FILE *saida1;
+    FILE *saida2;
+
+    saida1 = fopen("diag1.txt","w");
+
+    for(int i = 0; i<N; i++){
+        for (int j = 0; j<N; j++){
+                fprintf(saida1,"%lf",B[i][j]);
+        }
+    }
+
+    saida2 = fopen("diag2.txt","w");
+
+    for(int i = 0; i<N; i++){
+        for (int j = 0; j<N; j++){
+                fprintf(saida1,"%lf",C[i][j]);
+        }
+    }
+
+
+
+
 }
 
 int main (){
 	pthread_t threads[NumThread];
-	int i;
+	int i,j;
 	int arg[NumThread];
 	FILE *diag1;
-	
-	diag1 = fopen(,"r")
-	
-	fscanf("%d %d %s",&N, &T, &diag1); // entrada das dimensıes das matrizes, o numero de threads e do arquivo.
-	
-	
-	
-	
+
+
+	scanf("%d %d %s",&N, &T, NomeArq); // entrada das dimens√µes das matrizes, o numero de threads e do arquivo.
+
+	diag1 = fopen("diag1.txt","r");
+
+	rename("diag1.txt", NomeArq);
+
+	 for(i=0;i<N;i++){
+        for(j=0;j<10;j++){
+
+            fscanf(diag1,"%lf",&A[i][j]);
+
+            }
+	 }
+
 	for (i=0; i<T;i++){
 		arg[i]=i+1;
 		pthread_create(&threads[i], NULL,Div1,(void*)&arg[i]);
 		pthread_create(&threads[i], NULL,Div2,(void*)&arg[i]);
 		pthread_join(threads[i],NULL);
-	} 
+	}
 	printf("\n");
 
-	getchar();
-return 0;	
+return 0;
 }
+
 
 
 
