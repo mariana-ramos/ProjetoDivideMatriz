8b51352830bf3ef89ee5ee50d913b838c100a186
mariana-ramos <44540633+mariana-ramos@users.noreply.github.com>
2019-06-06 16:43:26 -0300

    Update TrabSO.c
    Código Final

--- TrabSO.c
+++ TrabSO.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#define MAX 1000
+#include <string.h>
+#include <time.h>
+
 
 /*----------------------------VARIÁVEIS GLOBAIS------------------------------------------------*/
-double A[MAX][MAX], B[MAX][MAX], C[MAX][MAX];
+double **A, **B, **C;
 int N; // dimensões da matriz
-int T; // Numero de threads
+int T=16; // Numero de threads
 char NomeArq[100];// nome do arquivo
 
 
 /*-------------------------------------------------------------------------------------------*/
 
+
 void *Div1(void *arg){  // Divide a matriz A e coloca a diagonal principal e acima na matriz B;
-    for(int i = 0; i<N;i++){
+
+ for(int i = 0; i<N;i++){
         for(int j=0;j<N;j++){
 
             if(j>i || j==i){
@@ -25,11 +29,12 @@ void *Div1(void *arg){  // Divide a matriz A e coloca a diagonal principal e aci
 
         }
     } 
-printf("B existe\n");
+
 }
 
 void *Div2(void *arg){  // Divide a matriz a e coloca a diagonal principal e abaixo na matriz C;
-	 for(int i = 0; i<N;i++){
+
+for(int i = 0; i<N;i++){
         for(int j=0;j<N;j++){
 
             if(i>j && i!=j){
@@ -41,31 +46,39 @@ void *Div2(void *arg){  // Divide a matriz a e coloca a diagonal principal e aba
 
         }
     }
-printf("C existe\n");
+
 }
 
 void GravaMatriznoArquivo(){
-    FILE *saida1;
-    FILE *saida2;
+	FILE *saida1;
+	FILE *saida2;
+ 	char NomeFinal1 [100];
+	char NomeFinal2 [100];
 
-    saida1 = fopen("AAAA.txt","w");
+	strcpy(NomeFinal1, NomeArq);
 
-    for(int i = 0; i<N; i++){
-        for (int j = 0; j<N; j++){
-                fprintf(saida1,"%lf",B[i][j]);
-        }
-    }
+	strcat(NomeFinal1,".diag1"); 
+     
+	saida1 = fopen(NomeFinal1,"w");
 
-    saida2 = fopen("BBBB.txt","w");
+	for(int i = 0; i<N; i++){
+        	for (int j = 0; j<N; j++){
+                	fprintf(saida1,"%lf",B[i][j]);
+        	}
+    	}
+	
+	strcpy(NomeFinal2, NomeArq);
 
-    for(int i = 0; i<N; i++){
-        for (int j = 0; j<N; j++){
-                fprintf(saida2,"%lf",C[i][j]);
-        }
-    }
+	strcat(NomeFinal2,".diag2");
 
-printf("Arquivos gravados se Deus quiser\n");
+	saida2 = fopen(NomeFinal2,"w");
 
+
+	for(int i = 0; i<N; i++){
+        	for (int j = 0; j<N; j++){
+                	fprintf(saida2,"%lf",C[i][j]);
+        	}
+    	}
 
 }
 
@@ -80,31 +93,59 @@ int main (){
 
 	Arq = fopen(NomeArq,"r");
 
+/*----------------------------------------------------MATRIZES-------------------------------------------------------*/
+A = (double **) malloc(N * sizeof(double *));//Alocação Matriz A
+  
+  
+for (i=0; i<N; i++)
+      A[i] = (double *) malloc(N * sizeof(double));
+
+B = (double **) malloc(N * sizeof(double *));//Alocação Matriz B
+  
+  
+for (i=0; i<N; i++)
+      B[i] = (double *) malloc(N * sizeof(double));
+
+C = (double **) malloc(N * sizeof(double *));//Alocação Matriz C
+  
+  
+for (i=0; i<N; i++)
+      C[i] = (double *) malloc(N * sizeof(double));
+/*--------------------------------------------------------------------------------------------------------------------*/
 
 	if(Arq == NULL){
 		printf("Erro de Leitura de Arquivo\n");
 	}
-else{
-	for(i=0;i<N;i++){
-        for(j=0;j<N;j++){
 
-            fscanf(Arq,"%lf",&A[i][j]);
+	else{
+		for(i=0;i<N;i++){
+	        for(j=0;j<N;j++){
+	
+	            fscanf(Arq,"%lf",&A[i][j]);
 
-            }
-	 }
+		}
+	}
 }
 		
 	 fclose(Arq);
 
-	for (int i=0; i<T/2;i++){// Laço para criar as threads e chamar a função Div1
+	clock_t ini = clock();// Para calcular o tempo em segundos
+
+	for (int i=0; i<T;i++){// Laço para criar as threads e chamar as funções de divisão
 		arg[i]=i+1;
 		pthread_create(&threads[i], NULL,Div1,(void*)&arg[i]);
 		pthread_create(&threads[i], NULL,Div2,(void*)&arg[i]);
+	}
+	
+	for (int i=0; i<T;i++){// Laço para criar as threads e chamar as funções de divisão
 		pthread_join(threads[i],NULL);
 	}
+	printf("\nFuncao executou em %f segundos\n", ((double)clock() - ini) / CLOCKS_PER_SEC);
 	
 	GravaMatriznoArquivo();//Chama a função que faz a saída.
 	printf("\n");
 
+
 return 0;
+
 }
